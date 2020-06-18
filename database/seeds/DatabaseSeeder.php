577876fd3d7ecce1d120e2a31d42e1b7e964c454
Frisco Nirwana Putra <37447797+frisconp@users.noreply.github.com>
2020-06-18 15:07:16 +0700

    Fix database config

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(RoleSeeder::class);
         $this->call(UserSeeder::class);
         $this->call(AdminSeeder::class);
+        $this->call(MenuCategorySeeder::class);
     }
 }
